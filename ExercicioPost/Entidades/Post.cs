1f62b3f61353a29bbe82af1d5f23da6caac469d4
Arthur Lorenzetti <tucaa222@gmail.com>
2021-04-01 15:20:52 -0300

    Adicionando exercicio com string builder

---
+++ ExercicioPost/Entidades/Post.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioPost.Entidades
+{
+    class Post
+    {
+        public DateTime MomentoDoPost { get; set; }
+        public string Titulo { get; set; }
+        public string Conteudo { get; set; }
+        public int Likes { get; set; }
+        public List<Comentario> Comentarios { get; set; } = new List<Comentario>();
+
+        public Post()
+        {
+
+        }
+
+        public Post(DateTime momentoPost, string titulo, string legenda, int likes)
+        {
+            MomentoDoPost = momentoPost;
+            Titulo = titulo;
+            Conteudo = legenda;
+            Likes = likes;
+        }
+
+        public void AddComentario(Comentario comentario)
+        {
+            Comentarios.Add(comentario);
+        }
+
+        public void RemoveComentario(Comentario comentario)
+        {
+            Comentarios.Remove(comentario);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(Titulo);
+            sb.Append(Likes);
+            sb.Append(" Likes - ");
+            sb.AppendLine(MomentoDoPost.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.AppendLine(Conteudo);
+            sb.AppendLine("Comentários: ");
+            foreach(Comentario c in Comentarios)
+            {
+                sb.AppendLine(c.TextoComentario);
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ ExercicioPost/Program.cs
@@ -0,0 +1,32 @@
+﻿using ExercicioPost.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioPost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Post post1 = new Post(DateTime.Parse("21/06/2018 13:05:44"), "Traveling to New Zealand",
+                "I'm going to visit this wonderful country!", 12); ;
+
+            post1.AddComentario(new Comentario("Have a nice trip!"));
+            post1.AddComentario(new Comentario("Wow, that's awesome!"));
+
+            Post post2 = new Post(DateTime.Parse("28/07/2018 23:14:19"), "Good night guys", 
+                "See you tomorrow", 5);
+
+            post2.AddComentario(new Comentario("Good night"));
+            post2.AddComentario(new Comentario("May the Force be with you"));
+
+            Console.WriteLine(post1);
+            Console.WriteLine(post2);
+
+            Console.ReadLine();
+        }
+    }
+}
