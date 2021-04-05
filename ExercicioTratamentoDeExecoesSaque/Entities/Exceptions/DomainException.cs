da42f6e955b00a2ea0fc7588918587eae472bf06
Arthur Lorenzetti <tucaa222@gmail.com>
2021-04-05 15:48:39 -0300

    Adicionando exercicio SaqueComTratamentoDeErros

--- ExercicioTratamentoDeExecoesSaque/Entities/Exceptions/DomainException.cs
+++ ExercicioTratamentoDeExecoesSaque/Entities/Exceptions/DomainException.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ExemploReservaHotel.Entities.Exceptions
+namespace ExercicioTratamentoDeExecoesSaque.Entities.Exceptions
 {
     class DomainException : ApplicationException
     {
---
+++ ExercicioTratamentoDeExecoesSaque/Program.cs
@@ -0,0 +1,50 @@
+﻿using ExercicioTratamentoDeExecoesSaque.Entities;
+using ExercicioTratamentoDeExecoesSaque.Entities.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioTratamentoDeExecoesSaque
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                Console.WriteLine("Enter accout data: ");
+                Console.Write("Number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                string holder = Console.ReadLine();
+                Console.Write("Initial Balance: ");
+                double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Withdraw limit: ");
+                double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Account account = new Account(number, holder, balance, withdrawLimit);
+                Console.WriteLine();
+
+                Console.Write("Enter amount for withdraw: ");
+                double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            try
+            {
+                account.Withdraw(amount);
+                Console.Write("New Balance: " + account.Balance.ToString("F2", CultureInfo.InvariantCulture));
+            }
+            catch(DomainException e)
+            {
+                Console.WriteLine("Withdraw error: " + e.Message);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Error unexpected: " + e.Message);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
