21681a4bda571581e5a30fd4bfb31b0bc8c145cc
Arthur Lorenzetti <tucaa222@gmail.com>
2021-04-05 15:12:47 -0300

    Adicionando Exemplo ReservaHotel Criando exceção personalizada

---
+++ ExemploReservaHotel/Program.cs
@@ -0,0 +1,54 @@
+﻿using ExemploReservaHotel.Entities;
+using ExemploReservaHotel.Entities.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploReservaHotel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Console.Write("Room Number: ");
+                int roomNumber = int.Parse(Console.ReadLine());
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                DateTime checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+                Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
+                Console.WriteLine("Reservation: " + reservation);
+                Console.WriteLine();
+
+                Console.WriteLine("Enter data to update the reservation: ");
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                checkOut = DateTime.Parse(Console.ReadLine());
+
+                reservation.UpdateDates(checkIn, checkOut);
+                Console.WriteLine("Reservation: " + reservation);
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine("Error in reservation: " + e.Message);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Format error: " + e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected error: " + e.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
